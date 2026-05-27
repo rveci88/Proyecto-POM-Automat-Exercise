00827f91fc3e535324d200f5b7c56e36e6e37b5c
rveci88 <rveci88@gmail.com>
2026-05-27 14:34:49 -0300

    prueba 3

--- POMtest/Apis/Utiles/Utils.ts
+++ POMtest/Apis/Utiles/Utils.ts
@@ -1,9 +1,14 @@
-const { faker } = require('@faker-js/faker/locale/es');
 
 export const ApiData={
     username:"admin",
     password:"password123",
     Base_ApiUrl:'https://restful-booker.herokuapp.com/',
-    firstname:()=> faker.person.firstName(),
-    lastname:()=> faker.person.lastName(),
+    firstname: async () => {
+        const { faker } = await import('@faker-js/faker/locale/es');
+        return faker.person.firstName()
+    },
+    lastname: async () => {
+        const { faker } = await import('@faker-js/faker/locale/es');
+        return faker.person.lastName()
+    }
 }
